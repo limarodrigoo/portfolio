c69e951a6c771d49905ce948d559b499973749b3
Rodrigo Lima <limarodrigoo@outlook.com>
2024-08-03 21:43:52 -0300

    chore: add profile front-end

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-"use client"
+"use client";
 
-import { Button } from "./_components/ui/button"
+import { Card, CardDescription, CardHeader } from "@components/ui/card";
+import TypingEffect from "./typeEffect";
 
 const Home = () => {
-  return <Button>test</Button>
-}
+  return (
+    <div className="grid h-dvh grid-cols-2 gap-5">
+      <Card className="flex flex-col justify-center">
+        <TypingEffect />
+      </Card>
+      <Card className="flex flex-col justify-center">
+        <CardHeader className="text-center text-3xl">Contact me:</CardHeader>
+        <CardDescription className="flex flex-col text-center">
+          <a
+            href="http://github.com/limarodrigoo"
+            target="_blank"
+            className="m-2 text-xl hover:text-foreground"
+          >
+            Github
+          </a>
+          <a
+            href="mailto:hello@limarodrigoo.com"
+            className="m-2 text-xl hover:text-foreground"
+          >
+            mail: hello@limarodrigoo.com
+          </a>
+        </CardDescription>
+      </Card>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
---
+++ app/typeEffect.tsx
@@ -0,0 +1,39 @@
+// import { randomUUID } from "crypto";
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+export default function TypingEffect() {
+  return (
+    <div className="text-center">
+      <Typewriter
+        // options={{ loop: true }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("Hello, World!")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("My name is Rodrigo.")
+            .pauseFor(1500)
+            .deleteAll()
+            .typeString("I'm a Back-end developer learning Front-end.")
+            .pauseFor(1500)
+            .typeString("<br>My main stacks are: ")
+            .typeString("<br> NodeJs")
+            .pauseFor(200)
+            .typeString("<br> Go,")
+            .pauseFor(200)
+            .typeString("<br> RabbitMQ,")
+            .pauseFor(200)
+            .typeString("<br> Redis")
+            .pauseFor(750)
+            .typeString(
+              "<br><br><br> Fell free to contact me at hello@limarodrigoo.com",
+            )
+            .pauseFor(12000)
+            .deleteAll()
+            .start();
+        }}
+      />
+    </div>
+  );
+}
