04457362d9cc5ba742938d1adc289e1c641f12b8
Rodrigo Lima <limarodrigoo@outlook.com>
2024-08-03 23:46:10 -0300

    refactor: move socials to a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
 "use client";
 
-import { Card } from "@components/ui/card";
 import TypingEffect from "./typeEffect";
-import GitLogo from "@/github-logo.png";
-import EmailLogo from "@/o-email.png";
-import Image from "next/image";
-import Link from "next/link";
+import Social from "./social";
 
 const Home = () => {
   return (
-    <div className="grid h-dvh">
-      <Card className="flex flex-col justify-center">
-        <TypingEffect />
-        <div className="my-10 flex justify-center">
-          <Link
-            href="http://github.com/limarodrigoo"
-            target="_blank"
-            className="m-2"
-          >
-            <Image src={GitLogo} alt="github" width={50} height={50}></Image>
-          </Link>
-          <Link
-            href="mailto:hello@limarodrigoo.com"
-            target="_blank"
-            className="m-2"
-          >
-            <Image src={EmailLogo} alt="email" width={50} height={50}></Image>
-          </Link>
-        </div>
-      </Card>
+    <div className="flex h-dvh flex-col justify-center">
+      <TypingEffect />
+      <Social />
     </div>
   );
 };
---
+++ app/social.tsx
@@ -0,0 +1,25 @@
+import GitLogo from "@/github-logo.png";
+import EmailLogo from "@/o-email.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Social() {
+  return (
+    <div className="my-10 flex justify-center">
+      <Link
+        href="http://github.com/limarodrigoo"
+        target="_blank"
+        className="m-2"
+      >
+        <Image src={GitLogo} alt="github" width={50} height={50}></Image>
+      </Link>
+      <Link
+        href="mailto:hello@limarodrigoo.com"
+        target="_blank"
+        className="m-2"
+      >
+        <Image src={EmailLogo} alt="email" width={50} height={50}></Image>
+      </Link>
+    </div>
+  );
+}
